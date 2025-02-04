3c5ad69d57e6c91318f7c4e3bd938e50d46caa6a
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-04 13:38:55 -0600

    Persistent storage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted, watch} from 'vue'
 
 const myArray = ref([]) 
 const name = ref('')
@@ -20,6 +20,23 @@ const addTodo = () =>{
   input_content.value = ''
   input_category.value = null
 }
+
+const removeTodo = (x) => {
+  myArray.value = myArray.value.filter(Element => Element !== x)
+} 
+
+onMounted( () => {
+  name.value = localStorage.getItem('name') || ''
+  myArray.value = JSON.parse(localStorage.getItem('myArray')) || [] 
+})
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+watch(myArray, (newVal) => {
+  localStorage.setItem('myArray', JSON.stringify(newVal))
+}, {deep: true}) 
 
 </script>
 
@@ -57,12 +74,18 @@ const addTodo = () =>{
     <section class="todo-list">
       <div class="list">
         <div v-for="x in myArray" :class="`todo-item ${x.done ? 'done' : 'not-done'}`" :key="x">
+          
           <label>
             <input type="checkbox" v-model="x.done"/>
             <span :class="`bubble ${x.category}`"></span>
           </label>
+          
           <div class="todo-content">
             <input type="text" v-model="x.content"/>
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(x)">Delete</button>
           </div>
 
         </div>
